68fdb3ec2b424119996f05d8c426bbc717ba9c46
LuisesCro <luis@crezcamosonline.com>
2026-09-10 16:39:44 -0500

    feat(imagenes): load Visual Stack builder

--- sophie-suite-nav.js
+++ sophie-suite-nav.js
@@ -68,7 +68,9 @@
         .then(function () { return cargar("https://ui.crezcamosonline.com/sophie-imagenes-research.js", "imagenes-research-v2"); })
         .then(function () { return cargar("https://ui.crezcamosonline.com/sophie-imagenes-research-ui.js", "imagenes-research-ui-v2"); })
         .then(function () { return cargar("https://ui.crezcamosonline.com/sophie-image-index.js", "image-index-v2"); })
-        .then(function () { return cargar("https://ui.crezcamosonline.com/sophie-image-index-ui.js", "image-index-ui-v2"); });
+        .then(function () { return cargar("https://ui.crezcamosonline.com/sophie-image-index-ui.js", "image-index-ui-v2"); })
+        .then(function () { return cargar("https://ui.crezcamosonline.com/sophie-visual-stack.js", "visual-stack-v2"); })
+        .then(function () { return cargar("https://ui.crezcamosonline.com/sophie-visual-stack-ui.js", "visual-stack-ui-v2"); });
     }
   } catch (e) {}
 })();
